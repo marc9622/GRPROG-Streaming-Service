157be33ee241552c4c94a4d06b74b263b57226cc
Christopher Robin Heldgaard <chris.heldgaard@gmail.com>
2022-12-08 16:44:58 +0100

    Update InformationPage.java

--- src/presentation/InformationPage.java
+++ src/presentation/InformationPage.java
@@ -1,9 +1,12 @@
 package presentation;
 
+import java.awt.Component;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
 
 public class InformationPage {
     
@@ -27,21 +30,30 @@ public class InformationPage {
       }
 
     private static void createAndShowInformationGUI() {
-        // Create a JFrame and set its content pane to a panel
-        JFrame playButtonFrame = new JFrame("Play video");
-        playButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
-        // Create a JButton with some text and add it to the panel
-        JButton playButton = new JButton("PLAY");
-        playButton.setBounds(100,100,100,100);
-        playButtonFrame.add(playButton);
+    // Create a JFrame and set its content pane to a panel
+    JFrame infoFrame = new JFrame("Information page");
+    infoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    // Create a JPanel and set its layout manager to BoxLayout
+    JPanel infoPanel = new JPanel();
+    infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.X_AXIS));
+
+    // Create some buttons and add them to the infoPanel
+    JButton playButton = new JButton("PLAY MEDIA");
+    JButton backButton = new JButton("GO BACK");
     
-        // Set the size and location of the frame and make it visible
-        playButtonFrame.setSize(800, 600);
-        playButtonFrame.setLocationRelativeTo(null);
-        playButtonFrame.setVisible(true);
+    infoPanel.add(Box.createHorizontalStrut(infoFrame.getHeight()/2));
+    infoPanel.add(Box.createVerticalStrut((infoFrame.getWidth())/2));
+    infoPanel.add(backButton);
+    infoPanel.add(playButton);
+
+    // Add the panel to the frame's content pane
+    infoFrame.add(infoPanel);
+
+    // Set the size and location of the frame and make it visible
+    infoFrame.setSize(800, 600);
+    infoFrame.setLocationRelativeTo(null);
+    infoFrame.setVisible(true);
 
       }
-    
 }

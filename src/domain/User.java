427820d90664397fed0d2de9b908d070d946652a
marc9622 <72046580+marc9622@users.noreply.github.com>
2022-12-08 16:00:25 +0100

    Added comparable implementation to User

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,6 +1,6 @@
 package domain;
 
-public class User {
+public class User implements Comparable<User> {
     
     private String username;
     private String password;
@@ -16,6 +16,15 @@ public class User {
 
     public boolean checkPassword(String password) {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /** Compares this user to the given user.
+     * @param user The user to compare.
+     * @return A negative integer, zero, or a positive integer
+     * as this user is less than, equal to, or greater than the given user.
+     */
+    public int compareTo(User user) {
+        return username.compareTo(user.username);
     }
 
 }
